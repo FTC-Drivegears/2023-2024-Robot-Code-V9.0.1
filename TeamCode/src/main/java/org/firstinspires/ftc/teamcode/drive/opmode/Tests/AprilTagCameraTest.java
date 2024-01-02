f2cbf67edf8cb2d72fb946905c5ac6128db82595
pekachoo <jliu6162@gmail.com>
2024-01-02 15:19:30 -0500

    Update AprilTagCameraTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/AprilTagCameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/AprilTagCameraTest.java
@@ -9,6 +9,9 @@ import android.graphics.Canvas;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicReference;
 import org.firstinspires.ftc.robotcore.external.function.Consumer;
 import org.firstinspires.ftc.robotcore.external.function.Continuation;
@@ -17,6 +20,7 @@ import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.VisionProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
@@ -37,13 +41,24 @@ public class AprilTagCameraTest extends LinearOpMode {
 
         packet = new TelemetryPacket();
 
-        while(opModeInInit()){
+        waitForStart();
+
+        while(!isStopRequested() && opModeIsActive()) {
             aprilCamSubsystem.runDetections();
-            telemetry.addData("Detections", aprilCamSubsystem.getDetections());
+            if(aprilCamSubsystem.getDetections().size() > 0) {
+                ArrayList<AprilTagDetection> detections = aprilCamSubsystem.getDetections();
+                telemetry.addData("Detections", detections);
+                for (int i = 0; i < detections.size(); i++) {
+                    telemetry.addData("x" + i, detections.get(i).ftcPose.x);
+                    telemetry.addData("y" + i, detections.get(i).ftcPose.y);
+                    telemetry.addData("z" + i, detections.get(i).ftcPose.z);
+                    telemetry.addData("yaw" + i, detections.get(i).ftcPose.yaw);
+                    telemetry.addData("pitch" + i, detections.get(i).ftcPose.pitch);
+                    telemetry.addData("roll" + i, detections.get(i).ftcPose.roll);
+                }
+            }
             telemetry.update();
         }
-
-        waitForStart();
 
     }
 
