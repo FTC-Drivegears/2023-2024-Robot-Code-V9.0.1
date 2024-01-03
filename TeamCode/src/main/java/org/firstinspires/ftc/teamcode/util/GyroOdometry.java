cc0f58f090a1dee714fd402b4bf6afc6fc77dc5d
pekachoo <jliu6162@gmail.com>
2024-01-02 19:54:39 -0500

    equation fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GyroOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GyroOdometry.java
@@ -59,10 +59,10 @@ public class GyroOdometry extends Specifications{
         vTheta = imuSubsystem.vTheta;
         totalfEncoder += bEncoderf;
         totalsEncoder += sEncoderf;
-        sEncoderf = odometrySubsystem.rightEncoder(); //s = side, b = back
+        sEncoderf = odometrySubsystem.leftEncoder(); //s = side, b = back
         bEncoderf = odometrySubsystem.backEncoder(); //dc = cm per tick, dyc = offset Y encoder, dxc = offset X encoder
-        dx = (sEncoderf-sEncoderi)*dc - dxc*dTheta;
-        dy = (bEncoderf-bEncoderi)*dc + dyc*dTheta;
+        dx = (sEncoderf-sEncoderi)*dc + dxc*dTheta;
+        dy = (bEncoderf-bEncoderi)*dc - dyc*dTheta;
         tempX = (dx*Math.cos(theta))+(dy*Math.sin(theta));
         tempY = (dy*Math.cos(theta))-(dx*Math.sin(theta));
         testx = (bEncoderf-bEncoderi)*dc;
