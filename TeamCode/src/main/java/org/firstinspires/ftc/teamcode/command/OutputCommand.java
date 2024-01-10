490dbf198913c2b885739e9e309159e4e971ca15
ConnorXTan <121638535+ConnorXTan@users.noreply.github.com>
2024-01-10 14:43:16 -0500

    Update OutputCommand.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/OutputCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/OutputCommand.java
@@ -69,8 +69,8 @@ public class OutputCommand {
     }
     public void armToIdle(){
         //TODO: Find value
-        leftArm.setPosition(0.295);
-        rightArm.setPosition(0.295);
+        leftArm.setPosition(0.29);
+        rightArm.setPosition(0.29);
     }
 
     public void armToBoard(){
@@ -79,8 +79,8 @@ public class OutputCommand {
     }
 
     public void tiltToIdle(){
-        leftTilt.setPosition(0.05);
-        rightTilt.setPosition(0.05);
+        leftTilt.setPosition(0.04);
+        rightTilt.setPosition(0.04);
     }
     public void tiltToBoard(){
         leftTilt.setPosition(0.92);
