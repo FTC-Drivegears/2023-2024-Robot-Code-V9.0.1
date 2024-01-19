51b09e6ec6b7f5e531f06df9aa64ebcfd4eefb98
pekachoo <jliu6162@gmail.com>
2024-01-19 13:53:31 -0500

    Create AllServoTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/AllServoTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="All Servo Test")
+public class AllServoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
