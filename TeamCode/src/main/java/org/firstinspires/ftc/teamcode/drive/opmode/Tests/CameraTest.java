47d2452f0ca475c6b0202bf5d502eaad1b67a77d
Thompson Li <thompson.li@mail.utoronto.ca>
2024-02-09 22:04:47 -0500

    Setup CameraTest for tuning -Thompson

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/CameraTest.java
@@ -29,10 +29,28 @@ public class CameraTest extends LinearOpMode {
             packet.put("x", camera.getContourProcessor().largestContourCenter().x);
             packet.put("y", camera.getContourProcessor().largestContourCenter().y);
             packet.put("area", camera.getContourProcessor().largestContourArea());
+            packet.put("Spike Position", findSpikePosition());
             telemetry.addData("x", camera.getContourProcessor().largestContourCenter().x);
             telemetry.addData("y", camera.getContourProcessor().largestContourCenter().y);
             telemetry.addData("area", camera.getContourProcessor().largestContourArea());
+            telemetry.addData("Spike Position", findSpikePosition());
             sleep(20);
         }
     }
+
+    /**
+     * Identifies what spike location the game piece is on.
+     *
+     * @return a String indication the location of the game piece
+     */
+    public String findSpikePosition() {
+        double center = camera.getXProp();
+
+        // TODO: Tune numbers so we can find the position of the spike
+        // For reference, camera is 864 pixels wide
+        return center < 288 ? "left"
+                : center < 576 ? "middle"
+                : "right";
+    }
+
 }
