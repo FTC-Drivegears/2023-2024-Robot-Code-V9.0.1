8af5f56b955484ba7df943f40e29b95420fca3d7
Thompson Li <thompson.li@mail.utoronto.ca>
2024-02-10 02:38:05 -0500

    Fixed camera stream, and can now show contours in Dashboard -Thompson

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Tests/CameraTest.java
@@ -6,34 +6,39 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.WebcamSubsystem;
-import org.openftc.apriltag.AprilTagDetection;
-
-import java.util.ArrayList;
+import org.opencv.core.Point;
 
 @TeleOp(name="camera test")
 public class CameraTest extends LinearOpMode {
     //https://docs.wpilib.org/en/stable/docs/software/vision-processing/grip/introduction-to-grip.html
-    private WebcamSubsystem camera;
-    private FtcDashboard dashboard;
-    private TelemetryPacket packet;
+    private WebcamSubsystem webcamSubsystem;
+
     @Override
-    public void runOpMode(){
-        camera = new WebcamSubsystem(hardwareMap, WebcamSubsystem.PipelineName.CONTOUR_BLUE);
-        dashboard = FtcDashboard.getInstance();
-        dashboard.startCameraStream(camera.webcam, 24);
-        packet = new TelemetryPacket();
+    public void runOpMode() {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        webcamSubsystem = new WebcamSubsystem(
+                hardwareMap,
+                WebcamSubsystem.PipelineName.CONTOUR_RED,
+                dashboard, gamepad1
+        );
+        dashboard.startCameraStream(webcamSubsystem.webcam, 24);
 
         waitForStart();
         while(opModeIsActive()){
-            dashboard.sendTelemetryPacket(packet);
-            packet.put("x", camera.getContourProcessor().largestContourCenter().x);
-            packet.put("y", camera.getContourProcessor().largestContourCenter().y);
-            packet.put("area", camera.getContourProcessor().largestContourArea());
+            /*
+            Go to http://192.168.43.1:8080/dash to see camera work
+             */
+            TelemetryPacket packet = new TelemetryPacket();
+            Point largestContourCenter = webcamSubsystem
+                    .getContourProcessor()
+                    .largestContourCenter();
+            packet.put("x", largestContourCenter.x);
+            packet.put("y", largestContourCenter.y);
+            packet.put("area", webcamSubsystem.getContourProcessor().largestContourArea());
             packet.put("Spike Position", findSpikePosition());
-            telemetry.addData("x", camera.getContourProcessor().largestContourCenter().x);
-            telemetry.addData("y", camera.getContourProcessor().largestContourCenter().y);
-            telemetry.addData("area", camera.getContourProcessor().largestContourArea());
-            telemetry.addData("Spike Position", findSpikePosition());
+            packet.put("Camera Status", webcamSubsystem.getCameraState());
+            packet.put("Button A Pressed?", gamepad1.a);
+            dashboard.sendTelemetryPacket(packet);
             sleep(20);
         }
     }
@@ -44,10 +49,10 @@ public class CameraTest extends LinearOpMode {
      * @return a String indication the location of the game piece
      */
     public String findSpikePosition() {
-        double center = camera.getXProp();
+        double center = webcamSubsystem.getXProp();
 
         // TODO: Tune numbers so we can find the position of the spike
-        // For reference, camera is 864 pixels wide
+        // For reference, the camera is 864 pixels wide
         return center < 288 ? "left"
                 : center < 576 ? "middle"
                 : "right";
